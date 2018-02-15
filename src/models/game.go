e518197c293213039a717c4eaaf2a81ed4b94a28
Raphael Parment <Raphael.Parment@macq.eu>
2018-02-15 23:24:34 +0100

    Implementation of Insert DbObject interface method, plus refactoring of models.

--- src/models/game.go
+++ src/models/game.go
@@ -1,9 +1,28 @@
 package models
 
+import (
+	"gopkg.in/olivere/elastic.v5"
+	"context"
+	"strconv"
+)
+
 type Game struct {
-	Date    string  `json:"date"`
-	Player1 *Player `json:"player1"`
-	Player2 *Player `json:"player2"`
-	Score1  int     `json:"score1"`
-	Score2  int     `json:"score2"`
+	DateTime string `json:"datetime"`
+	Player1  Player `json:"player1"`
+	Player2  Player `json:"player2"`
+	Score1   int    `json:"score1"`
+	Score2   int    `json:"score2"`
+}
+
+func (game Game) Insert(client *elastic.Client, ctx context.Context, id int) {
+	_, err := client.Index().
+		Index("games").
+		Type("doc").
+		Id(strconv.Itoa(id)).
+		BodyJson(game).
+		Do(ctx)
+
+	if err != nil {
+		panic(err)
+	}
 }
--- src/models/player.go
+++ src/models/player.go
@@ -1,7 +1,27 @@
 package models
 
+import (
+	"gopkg.in/olivere/elastic.v5"
+	"context"
+	"strconv"
+)
+
+
 type Player struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 	Points    int    `json:"points"`
+}
+
+func (player Player) Insert(client *elastic.Client, ctx context.Context, id int) {
+	_, err := client.Index().
+		Index("players").
+		Type("doc").
+		Id(strconv.Itoa(id)).
+		BodyJson(player).
+		Do(ctx)
+
+	if err != nil {
+		panic(err)
+	}
 }
