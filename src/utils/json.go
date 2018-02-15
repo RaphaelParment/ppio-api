3a786805cc4bb99ae0d4b7162e34674a87ca1632
Raphael Parment <Raphael.Parment@macq.eu>
2018-02-15 23:24:58 +0100

    GenerateGame method based on slice of players.

--- src/utils/json.go
+++ src/utils/json.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"os"
 	"io/ioutil"
-	"ppio-web/src/models"
+	"ppio/src/models"
+	"math/rand"
 )
 
 func GetPlayers() []models.Player {
@@ -18,4 +19,58 @@ func GetPlayers() []models.Player {
 	var c []models.Player
 	json.Unmarshal(raw, &c)
 	return c
+}
+
+func GenerateGames(players []models.Player) []models.Game {
+
+	var games []models.Game
+
+	for _, homePlayer := range players {
+		for _, awayPlayer := range players {
+
+			// generate a game
+			if homePlayer.FirstName != awayPlayer.FirstName {
+
+				// generate scores
+				homeScore := rand.Intn(20)
+				awayScore := rand.Intn(20)
+
+				if homeScore >= awayScore {
+					if awayScore > 15 {
+						awayScore = homeScore - 2
+					} else {
+						homeScore = 11
+						awayScore = rand.Intn(9) + 1
+					}
+				} else {
+					if homeScore > 15 {
+						homeScore = awayScore - 2
+					} else {
+						awayScore = 11
+						homeScore = rand.Intn(9) + 1
+					}
+				}
+
+				// generate random datetime (1 month span)
+				day := rand.Intn(31) + 1
+				hour := rand.Intn(24)
+				minute := rand.Intn(60)
+
+				datetime := fmt.Sprintf("2018-02-%d %d:%d:00",
+					day, hour, minute)
+
+				game := models.Game{
+					DateTime: datetime,
+					Player1: homePlayer,
+					Player2: awayPlayer,
+					Score1: homeScore,
+					Score2: awayScore,
+				}
+
+				games = append(games, game)
+			}
+		}
+	}
+
+	return games
 }
