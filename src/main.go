633154f18dc68171feb94f02eb220eedfc51ce51
Raphael Parment <Raphael.Parment@macq.eu>
2018-02-27 23:36:16 +0100

    Simplified version.

--- src/main.go
+++ src/main.go
@@ -9,21 +9,24 @@ import (
 	"ppio-web/utils"
 //	"time"
 
-	"github.com/coreos/go-systemd/daemon"
+//	"github.com/coreos/go-systemd/daemon"
 	"gopkg.in/olivere/elastic.v5"
+	"fmt"
+
 )
 
 
 /**
 Function which inserts dummy data into the database.
  */
-func initialiseDb(ctx context.Context) {
+func initialiseDb(ctx context.Context, client *elastic.Client) {
 
 	players := utils.GetPlayers()
 
 	for i, player := range players {
 		player.Insert(client, ctx, i)
 	}
+	fmt.Println("Players inserted")
 
 	games := utils.GenerateGames(players)
 
@@ -31,13 +34,14 @@ func initialiseDb(ctx context.Context) {
 
 		game.Insert(client, ctx, j)
 	}
+	fmt.Println("Games inserted")
 }
 
 
 func main() {
 
 	end := make(chan bool)
-	ctx := context.Background()
+	//ctx := context.Background()
 
 	client, err := elastic.NewClient(
                 elastic.SetURL("http://172.17.0.2:9200"),                           // Docker default public address for elasticsearch.
@@ -48,23 +52,23 @@ func main() {
         }
         defer client.Stop()
 
-	//initialiseDb(ctx)
+	//initialiseDb(ctx, client)
 
 	// Handle the routes with gorillamux
 	go func() {
 		http.ListenAndServe(":9000", routes.GetRouter(client))
 	}()
 
-	go func() {
-		interval, err := daemon.SdWatchdogEnabled(false)
-		if err != nil || interval == 0 {
-			log.Printf("Could not start the watchdog for SystemD...")
-		}
-		for {
-			daemon.SdNotify(false, "WATCHDOG=1")
-			time.Sleep(interval / 3)
-		}
-	}()
+	//go func() {
+	//	interval, err := daemon.SdWatchdogEnabled(false)
+	//	if err != nil || interval == 0 {
+	//		log.Printf("Could not start the watchdog for SystemD...")
+	//	}
+	//	for {
+	//		daemon.SdNotify(false, "WATCHDOG=1")
+	//		time.Sleep(interval / 3)
+	//	}
+	//}()
 
 	// TODO check where it could be sent ?
 	// Handle the termination of the program properly.
