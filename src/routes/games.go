e19a921ceaf02c7d08131b9526190868cc9312a1
Raphael Parment <Raphael.Parment@macq.eu>
2018-02-27 23:36:57 +0100

    New games endpoints.

--- src/routes/games.go
+++ src/routes/games.go
@@ -12,36 +12,129 @@ import (
 	"log"
 )
 
+func getGameHandler(client *elastic.Client) http.HandlerFunc {
+
+	fn := func(w http.ResponseWriter, req *http.Request) {
+
+		vars := mux.Vars(req)
+		ctx := context.Background()
+		gameID := vars["gameID"]
+		gameGet, err := client.
+			Get().
+			Index("games").
+			Type("doc").
+			Id(gameID).
+			Do(ctx)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		gameJSON, err := gameGet.Source.MarshalJSON()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(gameJSON)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
 func getGamesHandler(client *elastic.Client) http.HandlerFunc {
 
 	fn := func(w http.ResponseWriter, req *http.Request) {
 
-		var games []byte
+		var games []models.Game
 		ctx := context.Background()
-		docMatchQuery := elastic.NewTermQuery("_type", "doc")
-		scroller := client.
-			Scroll().
+
+		// Fetching count
+		count, err := client.Count().
 			Index("games").
 			Type("doc").
-			Query(docMatchQuery).
-			Sort("time", true).
-			Size(1)
+			Do(ctx)
 
-		docs := 0
-		for {
-			res, err := scroller.Do(ctx)
-			if err == io.EOF {
-				// No more documents
-				break
-			}
-			for _, hit := range res.Hits.Hits {
-				item := make(map[string]interface{})
-				_ := json.Unmarshal(*hit.Source, &item)
-				docs++
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Perform search
+		docMatchQuery := elastic.NewTermQuery("_type", "doc")
+		result, err := client.Search().
+			Index("games").
+			Query(docMatchQuery).
+			From(0).Size(int(count)).
+			Do(ctx)
+
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var game models.Game
+		for _, item := range result.Each(reflect.TypeOf(game)) {
+			if t, ok := item.(models.Game); ok {
+				game = models.Game{
+					DateTime: t.DateTime,
+					Player1: t.Player1,
+					Player2: t.Player2,
+					Score1: t.Score1,
+					Score2: t.Score2,
+				}
+				games = append(games, game)
 			}
 		}
+
 		w.Header().Set("Content-Type", "application/json")
-		w.Write(games)
+		json.NewEncoder(w).Encode(games)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func addGameHandler(client *elastic.Client) http.HandlerFunc {
+
+	fn := func(w http.ResponseWriter, req *http.Request) {
+
+		var game models.Game
+		ctx := context.Background()
+		reqBody, err := ioutil.ReadAll(req.Body)
+
+		if err != nil {
+			log.Println("Could not read request body while adding game.")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		err = json.Unmarshal(reqBody, &game)
+
+		// Making sure request body is well formatted
+		if err != nil {
+			log.Println("Request body does not match game structure")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		_, err = client.Index().
+			Index("games").
+			Type("doc").
+			BodyJson(game).
+			Refresh("true").
+			Do(ctx)
+
+		if err != nil {
+			log.Println("Could not insert game")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	}
 
 	return http.HandlerFunc(fn)
