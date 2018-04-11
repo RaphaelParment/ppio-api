d3fa0dad5cdcf21c63bc8b352c52128397960fbd
Raphish <raphparment@gmail.com>
2018-04-11 22:52:16 +0200

    Jobo, ça avance ppio ?

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,23 +1,31 @@
 CREATE TABLE game (
-id INT NOT NULL DEFAULT unique_rowid(),
-player1_id INTEGER NOT NULL,
-player2_id INTEGER NOT NULL,
-score1 INT NULL,
-score2 INT NULL,
-datetime TIMESTAMP WITH TIME ZONE NULL,
-CONSTRAINT "primary" PRIMARY KEY (id ASC),
-CONSTRAINT player1_fk FOREIGN KEY (player1_id) REFERENCES player (id),
-INDEX game_player1_id_idx (player1_id ASC),
-CONSTRAINT player2_fk FOREIGN KEY (player2_id) REFERENCES player (id),
-INDEX game_player2_id_idx (player2_id ASC),
-FAMILY "primary" (id, player1_id, player2_id, score1, score2, datetime)
+    id INT NOT NULL DEFAULT unique_rowid(),
+    player1_id INTEGER NOT NULL,
+    player2_id INTEGER NOT NULL,
+    datetime TIMESTAMP WITH TIME ZONE NULL,
+    CONSTRAINT "primary" PRIMARY KEY (id ASC),
+    CONSTRAINT player1_fk FOREIGN KEY (player1_id) REFERENCES player (id),
+    INDEX game_player1_id_idx (player1_id ASC),
+    CONSTRAINT player2_fk FOREIGN KEY (player2_id) REFERENCES player (id),
+    INDEX game_player2_id_idx (player2_id ASC),
+    FAMILY "primary" (id, player1_id, player2_id, datetime)
+);
+
+CREATE TABLE set (
+    id INT NOT NULL DEFAULT unique_rowid(),
+    game_id INT NOT NULL,
+    score1 INT NOT NULL DEFAULT 0,
+    score2 INT NOT NULL DEFAULT 0,
+    CONSTRAINT "primary" PRIMARY KEY (id ASC),
+    CONSTRAINT game_id_fk FOREIGN KEY (game_id) REFERENCES game(id),
+    FAMILY "primary" (id, game_id, score1, score2)
 );
 
 CREATE TABLE player (
-id INT NOT NULL DEFAULT unique_rowid(),
-first_name STRING NULL,
-last_name STRING NULL,
-points INT NULL,
-CONSTRAINT "primary" PRIMARY KEY (id ASC),
-FAMILY "primary" (id, first_name, last_name, points)
+    id INT NOT NULL DEFAULT unique_rowid(),
+    first_name STRING NULL,
+    last_name STRING NULL,
+    points INT NULL,
+    CONSTRAINT "primary" PRIMARY KEY (id ASC),
+    FAMILY "primary" (id, first_name, last_name, points)
 );
