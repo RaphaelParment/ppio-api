51e62ce66e6db34e3284dbcad2a472d3719fecf9
Jonathan Bodart <jon.bodart@gmail.com>
2018-04-23 21:45:56 +0200

    Prepare a query builder pattern.

--- models/abstract-model.go
+++ models/abstract-model.go
@@ -7,3 +7,26 @@ type IDbObject interface {
 	GetByID(dbConn *sql.DB) error
 	GetAll(dbConn *sql.DB) ([]IDbObject, error)
 }
+
+type Query interface {
+	From(tableName string)
+	Join(tableName string, jointColumnOrig string, jointColumnDest string)
+	Where(columnName string, value interface{})
+	ToSql() (string, error)
+}
+
+type JointTable struct {
+	TableName       string
+	JointColumnOrig string
+	JointColumnDest string
+}
+
+type WhereClause struct {
+	ColumnName string
+	value      interface{}
+}
+type PPIOQuery struct {
+	FromTable  string
+	JoinTables []JointTable
+	Where      []WhereClause
+}
