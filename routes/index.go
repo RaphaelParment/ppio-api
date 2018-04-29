3cb9dcb6efb92defa0eb9ed45daeaa4822fc656e
Jonathan Bodart <jon.bodart@gmail.com>
2018-04-29 17:17:35 +0200

    Method to parse values of limit and offset.

--- routes/index.go
+++ routes/index.go
@@ -2,10 +2,40 @@ package routes
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
+	"ppio/models"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
+
+func parseOffsetAndLimit(vars map[string]string) (int, int) {
+	limit, ok := vars["limit"]
+	var limitNr, offsetNr int
+	var err error
+	if ok {
+		limitNr, err = strconv.Atoi(limit)
+		if err != nil {
+			log.Printf("Could not parse the value of limit paramter. Err: %v", err)
+			limitNr = models.DefaultLimit
+		} else {
+			limitNr = models.DefaultLimit
+		}
+
+		offset, ok := vars["offset"]
+		if ok {
+			offsetNr, err = strconv.Atoi(offset)
+			if err != nil {
+				log.Printf("Could not parse the value of offset parameter. Err: %v", err)
+				offsetNr = models.DefaultOffset
+			}
+		} else {
+			offsetNr = models.DefaultOffset
+		}
+	}
+	return limitNr, offsetNr
+}
 
 // GetRouter Instantiate the router and mounts all the handler for the Zone30  routes.
 func GetRouter(dbConn *sql.DB) *mux.Router {
