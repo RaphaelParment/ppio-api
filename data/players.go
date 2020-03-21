987f078639cc964ce03c88fe30a10b20ce29d1a4
RaphaelParment <raphparment@gmail.com>
2020-03-21 18:27:27 +0100

    Replaced models by data package (perhaps not best name...).

---
+++ data/players.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type Player struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Points    int    `json:"points"`
+}
+
+type Players []*Player
+
+// func (p *Player) FromJSON(r io.Reader) error {
+// 	e := json.NewDecoder(r)
+// 	return e.Decode(p)
+// }
+
+func (p *Players) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func (p *Player) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
