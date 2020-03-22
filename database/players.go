986368de9eaff674b08e3bfb8e0d08c85378ba53
RaphaelParment <raphparment@gmail.com>
2020-03-22 14:53:40 +0100

    Database interfaction corresponding to the handlers.

--- database/players.go
+++ database/players.go
@@ -7,7 +7,7 @@ import (
 	"github.com/RaphaelParment/ppio-api/data"
 )
 
-// GetPlayers DB func
+// GetPlayers returns all players
 func GetPlayers(db *sql.DB) (data.Players, error) {
 	var players data.Players
 	rows, err := db.Query("SELECT * FROM player")
@@ -27,7 +27,7 @@ func GetPlayers(db *sql.DB) (data.Players, error) {
 	return players, nil
 }
 
-// GetPlayer returns the player with id specified in URL
+// GetPlayer returns the player with id <id>
 func GetPlayer(db *sql.DB, id int) (*data.Player, error) {
 	var p data.Player
 	q := "SELECT * FROM player WHERE id = $1"
@@ -40,4 +40,51 @@ func GetPlayer(db *sql.DB, id int) (*data.Player, error) {
 		return nil, fmt.Errorf("failed to query / cast for player id: %d", id)
 	}
 	return &p, nil
+}
+
+// AddPlayer inserts the player pointed to by <p>
+func AddPlayer(db *sql.DB, p *data.Player) error {
+	q := "INSERT INTO player (first_name, last_name, email, points) VALUES ($1, $2, $3, $4)"
+	_, err := db.Exec(q, p.FirstName, p.LastName, p.Email, p.Points)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdatePlayer updates the player with id <id> with the field of the player pointed to by <p>.
+// If there is no player with id <id>, false is returned as first parameter
+func UpdatePlayer(db *sql.DB, id int, p *data.Player) (bool, error) {
+	q := "UPDATE player SET first_name = $1, last_name = $2, email = $3, points = $4 WHERE id = $5"
+	res, err := db.Exec(q, p.FirstName, p.LastName, p.Email, p.Points, id)
+	if err != nil {
+		return false, err
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	if count == 0 {
+		return false, nil
+	}
+	return true, nil
+}
+
+// DeletePlayer removes the player with id <id>. If no player with id <id> is
+// found in the database, false is returned as first paramater
+func DeletePlayer(db *sql.DB, id int) (bool, error) {
+	q := "DELETE FROM player WHERE id = $1"
+	res, err := db.Exec(q, id)
+	if err != nil {
+		return false, err
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	if count == 0 {
+		return false, nil
+	}
+	return true, nil
 }
