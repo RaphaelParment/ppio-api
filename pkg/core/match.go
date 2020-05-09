c39b83c7dfbc0fa7b7db3f8d1993692bf9883d96
RaphaelParment <raphparment@gmail.com>
2020-05-09 03:15:36 +0200

    Refactoring match logic.

--- pkg/core/match.go
+++ pkg/core/match.go
@@ -4,19 +4,22 @@ import (
 	"encoding/json"
 	"io"
 	"time"
+
+	"github.com/go-playground/validator"
 )
 
-// Match contains information about a match between
-// a home player and an away player
+// KeyMatch is used as request context key
+type KeyMatch struct{}
+
+// Match contains information about a match
 type Match struct {
-	ID              int
-	HomePlayerID    int
-	AwayPlayerID    int
-	HomePlayerScore int
-	AwayPlayerScore int
-	Datetime        time.Time
+	ID             int       `json:"id"`
+	FirstPlayerID  int       `json:"first_player_id" validate:"required"`
+	SecondPlayerID int       `json:"second_player_id" validate:"required"`
+	Datetime       time.Time `json:"date_time" validate:"required"`
 }
 
+// Matches represents multiple matches
 type Matches []*Match
 
 // ToJSON returns the json representation of the match
@@ -25,6 +28,24 @@ func (m *Match) ToJSON(w io.Writer) error {
 	return e.Encode(m)
 }
 
+// FromJSON returns match representation of <r>
+func (m *Match) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(m)
+}
+
+// Validate checks the match contains correct field values
+func (m *Match) Validate() error {
+	validate := validator.New()
+	return validate.Struct(m)
+}
+
+// GetKey returns the request context key
+func (m *Match) GetKey() interface{} {
+	return KeyMatch{}
+}
+
+// ToJSON returns the json representation of multiple matches
 func (m *Matches) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(m)
